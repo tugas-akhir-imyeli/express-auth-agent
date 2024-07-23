cd5949883dc60c5ab18f390d50773b4ba94f99a4
Andreas Indra Kurniawan <andreasindrakurniawan@gmail.com>
2024-07-23 20:20:42 +0700

    Feat: add authority revocation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ export var router = express.Router();
 /* GET home page. */
 router.get("/", async function (req, res, next) {
   try{
-    res.send({ message: "Hello World" }).status(200);
+    res.redirect("/revocation")
   } catch(error:any){
     console.error(error.message);
     res.send({ message: "Internal Server Error", error: error }).status(500);
