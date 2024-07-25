a667d61b92962835fff49141fab2e19bab6823f9
Andreas Indra Kurniawan <andreasindrakurniawan@gmail.com>
2024-07-25 20:59:37 +0700

    Refactor: User Interface

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ const __dirname = path.dirname(__filename);
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(credentials);
 
